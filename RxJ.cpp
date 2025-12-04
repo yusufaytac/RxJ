f778bc37b54f8298647b77a7a3e8c1e4223ebb2d
yusufaytac <aytacyusuf6@gmail.com>
2025-12-04 21:35:21 +0300

    create window

---
+++ RxJ.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(400, 250, "Rai x Jin");
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        
+        EndDrawing();
+    }
+
+}
