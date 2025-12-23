ab6665f112c5ee52c3da2fd15de5ebcf36c0fbd9
yusufaytac <aytacyusuf6@gmail.com>
2025-12-23 14:36:57 +0300

    rai jump

--- main.cpp
+++ main.cpp
@@ -9,11 +9,17 @@ private:
     Texture2D rTexture = LoadTexture("textures/rai.png");
     Texture2D jTexture = LoadTexture("textures/jin.png");
 
-    
-    int raiVel = 150;
+   
 
-    const float jumpVel = 0.0f;
-    const float gravity = 0.0f;
+    Vector2 raiPos{-15, 170};
+    int raiVel = 250;
+    float raiJumpVel = 0.0f;
+    float gravity = 0.0f;
+
+    bool isRaiWalking = false;
+    bool isRaiOnGround = true;
+    bool isRaiInAir = false;
+
 
     struct AnimData{
         Rectangle rec;
@@ -23,31 +29,40 @@ private:
         float runningTime;
     };
     
-    // rai idle variables
+    // rai idle data
     AnimData raiIdle{
         {rTexture.width-rTexture.width/8 * 7, rTexture.height-rTexture.height/7*6, rTexture.width/8, rTexture.height/7}, // rectangle rec
-        {-15, 170}, // vector2 pos
+        raiPos, // vector2 pos
         1, // int frame
         1.0/7.0, // float update time
         0 // float running time
     };
 
-    // rai walk variables
+    // rai walk data
     AnimData raiWalk{
         {rTexture.width-rTexture.width/8 * 7, rTexture.height-rTexture.height/7*5, rTexture.width/8, rTexture.height/7},
-        {-15, 170},
+        raiPos,
         1,
         1.0/12.0,
         0.0
     };
 
-    
+    // rai jump data
+    AnimData raiJump{
+        {rTexture.width-rTexture.width/8 * 7, rTexture.height-rTexture.height/7*4, rTexture.width/8,rTexture.height/7},
+        {raiPos},
+        1,
+        1.0/4.0,
+        0.0
+    };
+
     float rightLeft = 1.0;
 
 };
 void Character::tick(float DeltaTime)
 {
-    bool isRaiWalking = false;
+    isRaiWalking = false;
+
     if(IsKeyDown(KEY_D))
     {
         rightLeft = 1.0;
@@ -58,36 +73,53 @@ void Character::tick(float DeltaTime)
         rightLeft = -1.0;
         isRaiWalking = true;
     }
-
-
-    // rai idle animation
-    if(!isRaiWalking)
+    if(IsKeyPressed(KEY_W) && !isRaiInAir)
     {
-        raiIdle.runningTime += DeltaTime;
-        if(raiIdle.runningTime >= raiIdle.updateTime)
+        isRaiWalking = false;
+        isRaiOnGround = false;
+        isRaiInAir = true;
+
+        raiJumpVel = -1000.0f;
+        gravity = 70.0;
+    }
+        
+    raiJumpVel += gravity;
+    raiPos.y = raiPos.y + (raiJumpVel * DeltaTime);
+    if(isRaiWalking)
+    {
+        raiPos.x = raiPos.x + (rightLeft * raiVel * DeltaTime);
+    }
+    if(raiPos.y >= 170)
+    {
+        raiPos.y = 170;
+        raiJumpVel = 0.0f;
+        gravity = 0.0f;
+        isRaiOnGround = true;
+        isRaiInAir = false;
+    }
+
+    if (isRaiInAir)
+    {   
+        raiJump.rec.width = rTexture.width/8 * rightLeft;
+
+        raiJump.runningTime = raiJump.runningTime + DeltaTime;
+        if(raiJump.runningTime >= raiJump.updateTime)
         {
-            raiIdle.runningTime = 0.0f;
-            raiIdle.rec.x = raiIdle.rec.width * raiIdle.frame;
-            raiIdle.frame ++;
-            if(raiIdle.frame > 4)
-            {
-                raiIdle.frame = 1;
-                
-            }
+            raiJump.runningTime = 0;
+            raiJump.rec.x = raiJump.frame * raiJump.rec.width * rightLeft;
+            raiJump.frame = raiJump.frame + 1;
+            if(raiJump.frame > 2) 
+                {
+                    raiJump.frame = 1; 
+                }
         }
+        DrawTextureRec(rTexture, raiJump.rec, raiPos, WHITE);
     }
-
-
-
-    if(!isRaiWalking)
-    {
-        DrawTextureRec(rTexture, raiIdle.rec, raiIdle.pos, WHITE);
-    }
-    else
+    else if (isRaiWalking)
     {
         // rai walk animation
         raiWalk.rec.width = rTexture.width/8 * rightLeft;
-        raiWalk.pos.x = raiWalk.pos.x + (rightLeft * raiVel * DeltaTime);
+        
         raiWalk.runningTime = raiWalk.runningTime + DeltaTime;
         raiIdle.pos = raiWalk.pos;
         if(raiWalk.runningTime >= raiWalk.updateTime && isRaiWalking == true)
@@ -101,8 +133,31 @@ void Character::tick(float DeltaTime)
             }
 
         }
-        DrawTextureRec(rTexture, raiWalk.rec, raiWalk.pos, WHITE);
+        DrawTextureRec(rTexture, raiWalk.rec, raiPos, WHITE);
     }
+    
+    else
+    {
+        // rai idle animation
+        raiIdle.rec.width = rTexture.width/8 * rightLeft;
+
+        raiIdle.runningTime += DeltaTime;
+        if(raiIdle.runningTime >= raiIdle.updateTime)
+        {
+            raiIdle.runningTime = 0.0f;
+            raiIdle.rec.x = raiIdle.rec.width * raiIdle.frame * rightLeft;
+            raiIdle.frame ++;
+            if(raiIdle.frame > 4)
+            {
+                raiIdle.frame = 1;
+                
+            }
+        }
+        DrawTextureRec(rTexture, raiIdle.rec, raiPos, WHITE);
+    }
+
+
+
 
 }
 
